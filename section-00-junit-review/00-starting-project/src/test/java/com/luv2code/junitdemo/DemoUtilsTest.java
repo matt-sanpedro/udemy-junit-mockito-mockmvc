96ba6f06951db4c1a21b98697f97dc0aa0eeefff
matt-sanpedro <mattsanpedro7@gmail.com>
2025-06-15 19:06:46 -0700

    Reran tests after reverting checkNull function and added notes on other common lifecyle methods

--- section-00-junit-review/00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
+++ section-00-junit-review/00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
@@ -4,6 +4,11 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+import java.beans.Transient;
 
 public class DemoUtilsTest {
     @Test
@@ -14,6 +19,17 @@ public class DemoUtilsTest {
         assertEquals(6, demoUtils.add(2, 4), "2+4 must be 6");
         assertNotEquals(6, demoUtils.add(1, 9), "1+9 must not be 6");
 
+    }
+
+    @Test
+    void testNullAndNotNull() {
+        DemoUtils demoUtils = new DemoUtils();
+
+        String str1 = null;
+        String str2 = "Hello";
+
+        assertNull(demoUtils.checkNull(str1), "String should be null");
+        assertNotNull(demoUtils.checkNull(str2), "String should not be null");
     }
 
 }
