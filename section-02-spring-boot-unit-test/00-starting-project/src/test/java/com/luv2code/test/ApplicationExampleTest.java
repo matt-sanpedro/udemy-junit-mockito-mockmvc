60b8d82b8caad942e5119e14e2b704d069ac6a0c
matt-sanpedro <mattsanpedro7@gmail.com>
2025-06-16 00:17:27 -0700

    Implemented basicTest with custom folder structure

---
+++ section-02-spring-boot-unit-test/00-starting-project/src/test/java/com/luv2code/test/ApplicationExampleTest.java
@@ -0,0 +1,15 @@
+package com.luv2code.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.luv2code.component.MvcTestingExampleApplication;
+
+@SpringBootTest(classes = MvcTestingExampleApplication.class)
+public class ApplicationExampleTest {
+    
+    @Test
+    void basicTest() {
+        
+    }
+}
